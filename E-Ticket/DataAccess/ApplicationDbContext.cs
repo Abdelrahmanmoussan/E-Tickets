b432f844c05278e8f8bd821717d11968927a2fa6
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2025-02-25 20:50:40 +0200

    add data access

---
+++ E-Ticket/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Ticket.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    => optionsBuilder.UseSqlServer("Data Source=MOUSSAN\\MSSQLSERVERS;Initial Catalog=E-Ticket;Integrated Security=True;TrustServerCertificate=True");
+    }
+}
